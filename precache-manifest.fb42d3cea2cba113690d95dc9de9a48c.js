69b9d9cd7ad0924292c1f48b1d41afb587fa64e4
masahiro8 <masahiro819@gmail.com>
2019-01-07 05:14:59 +0900

    Updates

---
+++ precache-manifest.fb42d3cea2cba113690d95dc9de9a48c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e3960fc27dcc5528dad",
+    "url": "/face_track/static/js/runtime~main.0e3960fc.js"
+  },
+  {
+    "revision": "a72c151226d0bef1bbd1",
+    "url": "/face_track/static/js/main.a72c1512.chunk.js"
+  },
+  {
+    "revision": "dce75b48f63b139badeb",
+    "url": "/face_track/static/js/1.dce75b48.chunk.js"
+  },
+  {
+    "revision": "a72c151226d0bef1bbd1",
+    "url": "/face_track/static/css/main.8642b352.chunk.css"
+  },
+  {
+    "revision": "7c4290cdb89a1e8b57b13413fa2dcaf6",
+    "url": "/face_track/index.html"
+  }
+];
